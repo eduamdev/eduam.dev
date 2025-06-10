8bfcf40e079b88e3d1309f8e38034f92daf93c8a
eduamdev <35645733+eduamdev@users.noreply.github.com>
2025-06-10 02:09:33 -0600

    Refactor Header component by removing social links and adjusting layout for improved styling and responsiveness.

--- app/header.tsx
+++ app/header.tsx
@@ -1,27 +1,17 @@
 import Image from 'next/image';
 import eduamdev from '@/assets/images/eduamdev.png';
 import { siteConfig } from '@/config/site';
-import { SocialLink } from '@/components/social-link';
-import {
-  IconBrandBluesky,
-  IconBrandGithub,
-  IconBrandX,
-} from '@tabler/icons-react';
 import TextFade from '@/components/text-fade';
 import { headers } from 'next/headers';
 import { MotionConfig } from 'motion/react';
 
 export async function Header() {
-  const {
-    name,
-    username,
-    links: { xcom, github, bluesky },
-  } = siteConfig;
+  const { name, username } = siteConfig;
 
   const nonce = (await headers()).get('x-nonce');
 
   return (
-    <header className="z-60 sticky top-0 flex min-h-[var(--header-height)] items-center justify-between gap-6 bg-neutral-50/80 px-5 backdrop-blur-sm md:px-9">
+    <header className="z-60 sticky top-0 flex min-h-[var(--header-height)] items-center bg-neutral-50/80 px-5 backdrop-blur-sm md:px-9">
       <div className="flex items-center gap-x-3">
         <Image
           src={eduamdev}
@@ -57,20 +47,6 @@ export async function Header() {
           </span>
         </div>
       </div>
-      <nav
-        aria-label="Social Links"
-        className="hidden items-center justify-center gap-x-2 sm:flex"
-      >
-        <SocialLink href={github.url} ariaLabel="Visit GitHub profile">
-          <IconBrandGithub />
-        </SocialLink>
-        <SocialLink href={xcom.url} ariaLabel="Visit Twitter profile">
-          <IconBrandX />
-        </SocialLink>
-        <SocialLink href={bluesky.url} ariaLabel="Visit Bluesky profile">
-          <IconBrandBluesky />
-        </SocialLink>
-      </nav>
     </header>
   );
 }
